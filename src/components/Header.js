f679d0e8a89ea0bd959717cd32088dcf73200481
Makentosh <ziki_007@ukr.net>
2020-03-08 11:08:42 +0200

    start way of the samurai 1.0

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="App__header">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/a/ad/Figma-1-logo.png" alt=""/>
+    </header>
+  )
+};
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <aside className="App__side">
+      <div>
+        <img src="https://s3.tproger.ru/uploads/2016/10/reactmini.png" alt=""/>
+      </div>
+      <div>
+        <div>
+          <img src="https://klike.net/uploads/posts/2019-03/1551511784_4.jpg" alt=""/>
+        </div>
+        <div>
+          <div>имя</div>
+          <div>date</div>
+          <div>city</div>
+          <div>education</div>
+          <div>website</div>
+        </div>
+      </div>
+
+    </aside>
+  )
+};
+
+export default Profile;
