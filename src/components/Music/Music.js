98a136c0a6e704da2243c9f22af007bac43fa106
Makentosh <ziki_007@ukr.net>
2020-04-12 17:51:17 +0300

    try dropzone music page

--- src/components/Music/Music.js
+++ src/components/Music/Music.js
@@ -1,28 +1,62 @@
 import React from 'react';
-import  classes from './Music.module.scss';
+import  './Music.scss';
 
 const Music = props => {
 
-
-
   let sendFile = (e) => {
+    let output = document.querySelector('.output');
     let input = e.target;
-    let fileReader = new FileReader();
 
-    fileReader.onload = () => {
-      // let dataURL = fileReader.result;
-      console.log()
-    };
 
-    console.log(fileReader)
+    for(let f = 0; f < input.files.length; f++) {
 
-    fileReader.readAsDataURL(input.files[0])
+      let reader = new FileReader();
+
+      reader.onload = () => {
+
+        let resultSrc = reader.result;
+
+        let img = document.createElement('img'),
+            div = document.createElement('div');
+
+            div.classList.add('thumbnail-wrap');
+            img.classList.add('thumbnail');
+            img.setAttribute('src', resultSrc);
+
+
+        div.insertAdjacentElement('afterbegin', img);
+        output.insertAdjacentElement('afterbegin', div)
+      };
+
+
+      reader.readAsDataURL(input.files[f]);
+
+    }
+
+
   };
 
+
+
   return (
-    <div className={classes.music}>
+    <div className="music">
       Music
-      <input type="file" accept="image/*" multiple  onChange={sendFile}/>
+
+      <div className="dropzone">
+        <div className="dropzone__text">
+          Перетягніть файл сюди
+        </div>
+        <input type="file"
+               className="field"
+               accept="image/*"
+               multiple
+               onChange={sendFile}/>
+      </div>
+
+      <div className="output">
+
+      </div>
+
     </div>
   )
 };
